a0065ef25cc9c299af0d9d78c1cd90842a5319c9
moe10291 <mohammad57zero@gmail.com>
2020-07-18 02:24:12 -0400

    updated and contactus

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+if(isset($_POST['submit'])){
+    $name= $_POST ['name'];
+    $mailFrom= $_POST ['email'];
+    $services= $_POST ['services'];
+    $subject= $_POST ['subject'];
+    $name= $_POST ['name'];
+
+    $mailTo= "mohammad_570@hotmail.com";
+    $headers= "From: ".$mailFrom;
+    $txt= "You have received an email from ".$name.".\n\n".$subject
+
+    mail($mailTo, $services, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
+?>
