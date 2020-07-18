13deec4882579fdaef601454b878db46056eeb1e
moe10291 <mohammad57zero@gmail.com>
2020-07-18 14:44:55 -0400

    Contact form working

--- contact.php
+++ contact.php
@@ -4,13 +4,13 @@ if(isset($_POST['submit'])){
     $mailFrom= $_POST ['email'];
     $services= $_POST ['services'];
     $subject= $_POST ['subject'];
-    $name= $_POST ['name'];
+     
 
     $mailTo= "mohammad_570@hotmail.com";
     $headers= "From: ".$mailFrom;
     $txt= "You have received an email from ".$name.".\n\n".$subject
 
     mail($mailTo, $services, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
+    header("Location: index.php?mailsent");
 }
 ?>
--- main.js
+++ main.js
@@ -1,4 +1,42 @@
+
 $(document).ready(function() {
+    $('.submit').click(function(event){
+        // event.preventDefault()
+        console.log('Its working');
+
+        let name= $('.name').val();
+        let mailFrom= $('.email').val();
+        let subject= $('.subject').val();
+        let message= $('.message').val();
+        let statusElm=$('.status')
+        statusElm.empty();
+
+        if(mailFrom.length > 5 && mailFrom.includes('@') && mailFrom.includes('.')){
+            statusElm.append('<div>email is valid</div>')
+        } else {
+            statusElm.append('<div>email is not valid</div>')
+        };
+
+        if(message.length > 10){
+            statusElm.append('<div>Message is valid</div>')
+        } else {
+            statusElm.append('<div>Message is not valid</div>')
+        }
+
+        if(name.length > 3){
+            statusElm.append('<div>Name is valid</div>')
+        } else {
+            statusElm.append('<div>Name is not valid</div>')
+        }
+
+        if(subject.length > 3){
+            console.log('<div>Name is valid</div>')
+        } else {
+            console.log('<div>Name is not valid</div>')
+        }
+
+
+    });
     $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
